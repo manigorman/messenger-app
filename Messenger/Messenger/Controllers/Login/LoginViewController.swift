9312185a4a00cbd585fee96128bf4836d7666691
manigorman <immanakov@gmail.com>
2021-09-14 16:59:53 +0300

    Added Firebase authorization.

--- Messenger/Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -134,6 +135,14 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase Log In
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email: \(email)")
+                return
+            }
+            let user = result.user
+            print("Logged In User: \(user)")
+        }
     }
     
     func alertUserLoginError() {
