fcf27c9bb4a6fd1c4a0b223aa18cc95f74193675
manigorman <immanakov@gmail.com>
2021-09-15 22:09:56 +0300

    Add Profile and Chats into navigation controller.

--- Messenger/Messenger/Controllers/ConversationViewController.swift
+++ Messenger/Messenger/Controllers/ConversationViewController.swift
@@ -13,7 +13,7 @@ class ConversationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red // Фон нашего первоначального контроллера
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
