dd693896240cc3e99b08b06e62fb5d1df8d58bb4
manigorman <immanakov@gmail.com>
2022-03-18 22:29:06 +0300

    Set up table with users in NewConversationVC

--- Messenger/Controllers/NewConversationVC.swift
+++ Messenger/Controllers/NewConversationVC.swift
@@ -14,6 +14,11 @@ class NewConversationVC: UIViewController {
     
     private let spinner = JGProgressHUD()
     
+    private var users = [[String: String]]()
+    private var results = [[String: String]]()
+    
+    private var hasFetched = false
+    
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Search for Users..."
@@ -25,6 +30,7 @@ class NewConversationVC: UIViewController {
         let table = UITableView()
         table.isHidden = true
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
         
         return table
     }()
@@ -36,6 +42,7 @@ class NewConversationVC: UIViewController {
         label.textAlignment = .center
         label.textColor = .gray
         label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
@@ -46,7 +53,9 @@ class NewConversationVC: UIViewController {
         super.viewDidLoad()
         
         setupViews()
+        setConstraints()
         setupDelegates()
+        
         searchBar.becomeFirstResponder()
     }
     
@@ -59,10 +68,29 @@ class NewConversationVC: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(dismissSelf))
+        view.addSubview(noResultsLabel)
+        view.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            noResultsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            noResultsLabel.heightAnchor.constraint(equalToConstant: 40),
+            noResultsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            noResultsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
     
     private func setupDelegates() {
         searchBar.delegate = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     // MARK: - Selectors
@@ -75,7 +103,89 @@ class NewConversationVC: UIViewController {
 extension NewConversationVC: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
         
+        searchBar.resignFirstResponder()
+        
+        results.removeAll()
+        
+        spinner.show(in: view)
+        
+        self.searchUsers(query: text)
     }
     
+    func searchUsers(query: String) {
+        // check if array has firebase results
+        if hasFetched {
+            // if it does: filter
+            filterUsers(with: query)
+        } else {
+            // if not, fetch then filter
+            DatabaseManager.shared.getAllUsers { [weak self] result in
+                switch result {
+                case .success(let usersCollection):
+                    self?.hasFetched = true
+                    self?.users = usersCollection
+                    self?.filterUsers(with: query)
+                case .failure(let error):
+                    print("Failed to get users: \(error)")
+                }
+            }
+        }
+        
+        // update the UI: either show results or show no results label
+    }
+    
+    func filterUsers(with term: String) {
+        guard hasFetched else {
+            return
+        }
+        
+        self.spinner.dismiss()
+        let results: [[String: String]] = self.users.filter({
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            
+            return name.hasPrefix(term.lowercased())
+        })
+        
+        self.results = results
+        
+        self.updateUI()
+    }
+    
+    func updateUI() {
+        if results.isEmpty {
+            self.noResultsLabel.isHidden = false
+            self.tableView.isHidden = true
+        } else {
+            self.noResultsLabel.isHidden = true
+            self.tableView.isHidden = false
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+}
+
+extension NewConversationVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //start conversation
+    }
 }
---
+++ Messenger/Resources/Extensions/NewConversationVC+TableView.swift
@@ -0,0 +1,10 @@
+//
+//  NewConversationVC+TableView.swift
+//  Messenger
+//
+//  Created by Igor Manakov on 18.03.2022.
+//
+
+import Foundation
+import UIKit
+
